6ba82e99f06b573696cc8b8a8664fb6a63e79af5
Tomas Trnka <trnkatomas@outlook.com>
2024-03-16 18:04:48 +0100

    úpravy v 4 a 5 chapter

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,10 @@
+export default function Page() {
+  return (
+    <div>
+      <h1>Customers</h1>
+      <p>Matoušek je malinkej a Jáchym je ještě menší</p>
+      <p>Táta není futřík</p>
+      neco dalšího ještě něco
+    </div>
+  );
+}
